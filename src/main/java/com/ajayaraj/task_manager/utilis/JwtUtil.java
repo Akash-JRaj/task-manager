2c124067541d67db05af65b06862abd4a7fc93a4
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-08 18:39:16 +0530

    Modified JwtUtil.

--- src/main/java/com/ajayaraj/task_manager/utilis/JwtUtil.java
+++ src/main/java/com/ajayaraj/task_manager/utilis/JwtUtil.java
@@ -1,11 +1,13 @@
-package com.ajayaraj.task_manager.config;
+package com.ajayaraj.task_manager.utilis;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
@@ -27,6 +29,30 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUserName(String token) {
+        return extractClaims(token)
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String userName) {
+        String extractedUserName = extractUserName(token);
+        return !isTokenExpired(token) && (extractedUserName.equals(userName));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token)
+                .getExpiration().before(new Date());
     }
 
 }
