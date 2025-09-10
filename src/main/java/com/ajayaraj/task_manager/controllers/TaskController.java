4dab0b1d6f1209ca35224211366cde34b271fa0e
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-10 23:35:05 +0530

    Added endpoints for getting allTasks, creating tasks, getting task by task id

--- src/main/java/com/ajayaraj/task_manager/controllers/TaskController.java
+++ src/main/java/com/ajayaraj/task_manager/controllers/TaskController.java
@@ -1,5 +1,6 @@
 package com.ajayaraj.task_manager.controllers;
 
+import com.ajayaraj.task_manager.dtos.TaskDto;
 import com.ajayaraj.task_manager.models.Task;
 import com.ajayaraj.task_manager.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,20 +18,23 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable UUID taskId) {
-        Task task = taskService.getTaskByTaskId(taskId);
-
-        if(task == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
-        return ResponseEntity.status(HttpStatus.FOUND).body(task);
-    }
-
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks() {
+        return ResponseEntity.status(HttpStatus.OK).body(taskService.getTasks());
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<TaskDto> getTaskByTaskId(@PathVariable UUID taskId) {
+        if(!taskService.isCurrentUserIsTheOwnerOfTask(taskId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(taskService.getTaskByTaskId(taskId));
     }
 
 }
